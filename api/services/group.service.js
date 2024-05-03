e3af0fbcecdc758ff543cb25c66d6df2cf35e8a0
nteguem roland <nteguemroland@gmail.com>
2024-05-03 12:00:02 +0100

    review

--- api/services/group.service.js
+++ api/services/group.service.js
@@ -37,7 +37,7 @@ async function deleteGroup(groupId) {
 
 async function listGroups() {
     try {
-        const groups = await Group.find({}, { __v:0 });
+        const groups = await Group.find({}, { __v:0 }).populate('members');
         return { success: true, groups };
     } catch (error) {
         return { success: false, error: error.message };
